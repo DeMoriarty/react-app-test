eec13741ce607345c75a85c3fe61995677cd1893
DeMoriarty <sn591771872@vip.qq.com>
2019-03-06 23:00:59 +0300

    Updates

---
+++ precache-manifest.05056f361b73dbf81c6293cabdb77e8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "51edb1b991b292d725b0",
+    "url": "/react-app-test/static/js/runtime~main.51edb1b9.js"
+  },
+  {
+    "revision": "deee31681d304286cbdd",
+    "url": "/react-app-test/static/js/main.deee3168.chunk.js"
+  },
+  {
+    "revision": "8f4d33cc214c81cf14a9",
+    "url": "/react-app-test/static/js/2.8f4d33cc.chunk.js"
+  },
+  {
+    "revision": "deee31681d304286cbdd",
+    "url": "/react-app-test/static/css/main.f4cff842.chunk.css"
+  },
+  {
+    "revision": "d274fa06a523e1926221b362ca343649",
+    "url": "/react-app-test/index.html"
+  }
+];
